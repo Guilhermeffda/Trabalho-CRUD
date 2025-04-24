f9588854e839c73ad4b896977b58d2066b74f71e
Guilherme Ferraz <132021358+Guilhermeffda@users.noreply.github.com>
2025-04-24 12:04:15 -0300

    Update DataList.jsx

--- frontend/src/components/DataList.jsx
+++ frontend/src/components/DataList.jsx
@@ -22,31 +22,40 @@ const DataList = () => {
   };
 
   return (
-    <div className="card">
-      <h1 className="title">Listando Usuários</h1>
+    <div className="page-container">
+      <div className="content-wrap">
+        <div className="card">
+          <h1 className="title">Listando Usuários</h1>
           <div className="btn-adicionar"> 
             <button onClick={() => navigate("/adicionar")} className="btn-list">
-            Adicionar Usuário
+              Adicionar Usuário
             </button>
           </div>
-      <ul className="card_list">
-        {data.map((item) => (
-          <li key={item.id} className="li-list">
-            Nome: {item.nome}<br />
-            Idade: {item.idade}<br />
-            CPF: {item.cpf}<br />
-            <button onClick={() => navigate(`/detalhes/${item.id}`)} className="btn-list">
-              Ver mais detalhes
-            </button>
-            <button onClick={() => navigate(`/editar/${item.id}`)} className="btn-list">
-              Editar
-            </button>
-            <button onClick={() => handleDelete(item.id)} className="btn-list">
-              Deletar
-            </button>
-          </li>
-        ))}
-      </ul>
+          <ul className="card_list">
+            {data.map((item) => (
+              <li key={item.id} className="li-list">
+                Nome: {item.nome}<br />
+                Idade: {item.idade}<br />
+                CPF: {item.cpf}<br />
+                <button onClick={() => navigate(`/detalhes/${item.id}`)} className="btn-list">
+                  Ver mais detalhes
+                </button>
+                <button onClick={() => navigate(`/editar/${item.id}`)} className="btn-list">
+                  Editar
+                </button>
+                <button onClick={() => handleDelete(item.id)} className="btn-list">
+                  Deletar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      
+      <footer className="rodape">
+        <p>© {new Date().getFullYear()} Site feito por Guilherme Ferraz Freire De Araujo.</p>
+        <a href="https://github.com/Guilhermeffda/Trabalho-CRUD">Clique aqui para ir ao GitHub do projeto</a>
+      </footer>
     </div>
   );
 };
