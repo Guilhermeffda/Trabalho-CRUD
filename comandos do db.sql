f5b33a0e636708d601d37e3ff8df0cfde9ce0b24
Guilherme Ferraz <132021358+Guilhermeffda@users.noreply.github.com>
2025-04-24 12:24:03 -0300

    Update comandos do db.sql

--- comandos do db.sql
+++ comandos do db.sql
@@ -1,4 +1,8 @@
 
+
+CREATE DATABASE IF NOT EXISTS site_web;
+USE site_web;
+
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -9,27 +13,13 @@ CREATE TABLE usuarios (
 );
 
 INSERT INTO usuarios (nome, idade, cpf, email, senha) VALUES 
-('Collier Group', '35', NULL, 'Savannah_Moore33@gmail.com', 'Fyy_fzzJjsLGg_B'),
-('Cormier and Sons', '42', NULL, 'Willa.Hansen@hotmail.com', 'HnNyYHNw_iLzXBy'),
-('Shanahan, Schmeler and Walsh', '28', NULL, 'Orie_Konopelski39@yahoo.com', '2RelSiO9EpJLX1V'),
-('Mayer, Thompson and Pfannerstill', '51', NULL, 'Daniela.Jerde37@yahoo.com', 'wg4_iCApirgPRvr'),
-('Veum LLC', '39', NULL, 'Duncan.Wiegand60@gmail.com', 'UfHALM2YmlNZsLP'),
-('Bailey, Fahey and Goldner', '47', NULL, 'Javier_Ullrich14@yahoo.com', 'triYgTnYOlv7BDn'),
-('Anderson - Greenfelder', '33', NULL, 'Dayton_Moen80@hotmail.com', 'l7IXwN_YlND7xB8'),
-('Keeling LLC', '29', NULL, 'Alvena_Deckow@gmail.com', 'YLgArW70wz_D_11'),
-('Stracke Group', '45', NULL, 'Robyn85@hotmail.com', 'ZZ7vGvKENMFJ5vG'),
-('Hackett, Labadie and Ondricka', '31', NULL, 'Winfield_Hyatt@yahoo.com', '8ruVWG5EY3qrx3J');
-
-
-UPDATE usuarios SET cpf = '52998224725' WHERE id = 1;
-UPDATE usuarios SET cpf = '45317828890' WHERE id = 2;
-UPDATE usuarios SET cpf = '78452963107' WHERE id = 3;
-UPDATE usuarios SET cpf = '93647152834' WHERE id = 4;
-UPDATE usuarios SET cpf = '21589346752' WHERE id = 5;
-UPDATE usuarios SET cpf = '69832415709' WHERE id = 6;
-UPDATE usuarios SET cpf = '32758694103' WHERE id = 7;
-UPDATE usuarios SET cpf = '86142973568' WHERE id = 8;
-UPDATE usuarios SET cpf = '49382715604' WHERE id = 9;
-UPDATE usuarios SET cpf = '76519284357' WHERE id = 10;
-
-
+('Collier Group', 35, '52998224725', 'Savannah_Moore33@gmail.com', 'Fyy_fzzJjsLGg_B'),
+('Cormier and Sons', 42, '45317828890', 'Willa.Hansen@hotmail.com', 'HnNyYHNw_iLzXBy'),
+('Shanahan, Schmeler and Walsh', 28, '78452963107', 'Orie_Konopelski39@yahoo.com', '2RelSiO9EpJLX1V'),
+('Mayer, Thompson and Pfannerstill', 51, '93647152834', 'Daniela.Jerde37@yahoo.com', 'wg4_iCApirgPRvr'),
+('Veum LLC', 39, '21589346752', 'Duncan.Wiegand60@gmail.com', 'UfHALM2YmlNZsLP'),
+('Bailey, Fahey and Goldner', 47, '69832415709', 'Javier_Ullrich14@yahoo.com', 'triYgTnYOlv7BDn'),
+('Anderson - Greenfelder', 33, '32758694103', 'Dayton_Moen80@hotmail.com', 'l7IXwN_YlND7xB8'),
+('Keeling LLC', 29, '86142973568', 'Alvena_Deckow@gmail.com', 'YLgArW70wz_D_11'),
+('Stracke Group', 45, '49382715604', 'Robyn85@hotmail.com', 'ZZ7vGvKENMFJ5vG'),
+('Hackett, Labadie and Ondricka', 31, '76519284357', 'Winfield_Hyatt@yahoo.com', '8ruVWG5EY3qrx3J');
